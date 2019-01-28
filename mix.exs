605a6c3d40abdfa2c52c7dfb340361b57dc145fa
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2019-01-28 13:10:13 +0100

    fix app name

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExMicrosoftAzureUtils.MixProject do
 
   def project do
     [
-      app: :ex_microsoft_azure_management_samples,
+      app: :ex_microsoft_azure_utils,
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
